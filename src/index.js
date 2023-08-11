3c684d3cdb5d40e3780fad9d86edc20e7f490da4
MakotoTaguchi1 <makoto.taguchi1@x-factory.jp>
2023-08-11 16:04:08 +0900

    refactor: reducerをファイル分割

--- src/index.js
+++ src/index.js
@@ -2,40 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {createStore} from "redux"
+import { createStore } from 'redux';
+import allReducers from './reducers';
 
-// actions -> increment, decremant
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-}
-
-const decremant = () => {
-  return {
-    type: "DECREMENT",
-  };
-}
-
-// reducer: actionと前の状態を組み合わせて新しい状態に更新する
-const counterReducer = (state = 0, action)  => {
-  switch(action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-}
-
-// store
-let store = createStore(counterReducer)
-
-// consoleに新しい状態を出力
-store.subscribe(() => console.log(store.getState))
-
-// dispatch
-store.dispatch(increment())
-store.dispatch(increment())
+createStore = createStore(allReducers)
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
