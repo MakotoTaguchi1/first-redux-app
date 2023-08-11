fb81ac71f764e963d386a612ed01b58a4adb517a
MakotoTaguchi1 <makoto.taguchi1@x-factory.jp>
2023-08-11 17:46:16 +0900

    feat: dispatchとactionsによりボタン押下時の処理実装

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement, login } from './actions';
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const isLogin = useSelector((state) => state.isLogin);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <h1>Hello Redux</h1>
       <h3>カウント: {counter}</h3>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
       {isLogin ? <h3>ログイン成功！</h3> : <h3>ログインしてください</h3>}
+      <button onClick={() => dispatch(login())}>ログインorログアウト</button>
     </div>
   );
 }
