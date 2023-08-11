48ac3244c39e80597367044b7ada9d7f51902db8
MakotoTaguchi1 <makoto.taguchi1@x-factory.jp>
2023-08-11 15:36:12 +0900

    feat: increment & decrement

--- src/index.js
+++ src/index.js
@@ -2,7 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux"
+
+// actions -> increment, decremant
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+}
+
+const decremant = () => {
+  return {
+    type: "DECREMENT",
+  };
+}
+
+// reducer: actionと前の状態を組み合わせて新しい状態に更新する
+const counterReducer = (state = 0, action)  => {
+  switch(action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+}
+
+// store
+let store = createStore(counterReducer)
+
+// consoleに新しい状態を出力
+store.subscribe(() => console.log(store.getState))
+
+// dispatch
+store.dispatch(increment())
+store.dispatch(increment())
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +44,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
